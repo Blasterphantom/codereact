2e14dc462844cb1e542949d42e14cf497c43a34f
Blasterphantom <blasterneko@gmail.com>
2023-05-02 09:22:48 -0700

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import HomeComponent from './Components/HomeComponent/HomeComponent.js';
+import LoginComponent from './Components/LoginComponent/LoginComponent.js'
+import CreateAccountComponent from './Components/CreateAccountComponent/CreateAccountComponent.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id='startPage' className='App'>
+        <Routes>
+              <Route path="/" element={<HomeComponent />} />
+              <Route path="Login" element={<LoginComponent/> } />
+              <Route path="CreateAccount" element={<CreateAccountComponent/> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
