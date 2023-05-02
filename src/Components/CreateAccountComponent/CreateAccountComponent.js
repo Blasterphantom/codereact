864c5ecad7503448cb93a6cfeb7c980c4b3885f2
Harrison <wilkinsnigel2000@gmail.com>
2023-05-02 11:51:21 -0700

    create a user! done

--- src/Components/CreateAccountComponent/CreateAccountComponent.js
+++ src/Components/CreateAccountComponent/CreateAccountComponent.js
@@ -1,7 +1,63 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Row
+} from 'react-bootstrap'
+import './Create.css'
 export default function CreateAccountComponent() {
+
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    let userData = {
+      Username : Username,
+      Password : Password
+    }
+    console.log(userData)
+  }
   return (
-    <div>CreateAccountComponent</div>
+    <div className='form-controller'>
+      < Container >
+      <Row>
+        <h1>Create an Account!</h1>
+        <Col style={{backgroundColor: 'grey'}}>
+
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Username</Form.Label>
+        <Form.Control 
+        type="text" 
+        placeholder="Enter username" 
+        onChange={({target : {value}}) => setUsername(value)}/>
+
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control 
+        type="password"
+        placeholder="Password"
+        onChange={({target : {value}}) => setPassword(value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button 
+      variant="primary" 
+      // type="submit"
+      onClick={handleSubmit}
+      >
+        Submit
+      </Button>
+    </Form>
+
+        </Col>
+      </Row>
+      </Container>
+    </div>
   )
 }
