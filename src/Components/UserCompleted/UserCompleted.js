6c762322146f0f9f1fd631186ae713aa608526ab
Blasterphantom <blasterneko@gmail.com>
2023-05-03 12:15:16 -0700

    Filter, Nav, UserBar Components init

---
+++ src/Components/UserCompleted/UserCompleted.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../UserCompleted/UserCompleted.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import UserBar from '../UserBar/UserBar';
+import NavRow from '../NavRow/NavRow';
+import Filter from '../FilterComponent/Filter';
+
+export default function UserCompleted() {
+  return (
+    <Container fluid className='completedContainer'>
+        <Row className='completedRow'>
+            <Button className='logOutBtn'/>
+            <h4 className='logOutTxt'>Logout</h4>
+        </Row>
+
+        <UserBar />
+
+        <NavRow />
+
+        <Row className='filterRow'>
+            <div className='width82'>
+                <Filter />
+            </div>
+        </Row>
+
+    </Container>
+  )
+}
