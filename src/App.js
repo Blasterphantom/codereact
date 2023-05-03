8129326a787ad088814317b5129c7bf100800588
jayStackk <jmagpantay@codestack.co>
2023-05-03 12:15:08 -0700

    quickpushh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import HomeComponent from './Components/HomeComponent/HomeComponent.js';
 import LoginComponent from './Components/LoginComponent/LoginComponent.js'
 import CreateAccountComponent from './Components/CreateAccountComponent/CreateAccountComponent.js';
+import LoginMe from './Components/LoginComponent/LoginComponent.js';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div id='startPage' className='App'>
-        <Routes>
-              <Route path="/" element={<HomeComponent />} />
-              <Route path="Login" element={<LoginComponent/> } />
-              <Route path="CreateAccount" element={<CreateAccountComponent/> } />
-        </Routes>
-      </div>
-    </BrowserRouter>
+   <>
+   <LoginMe/>
+   </>
   );
 }
 
--- src/Components/LoginComponent/LoginComponent.js
+++ src/Components/LoginComponent/LoginComponent.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import { useState } from "react";
+import { FloatingLabel, Form, Container } from "react-bootstrap";
+import './Login.css'
 
-export default function LoginComponent() {
+// import jpIMG from "./assets/jp.svg";
+
+import "../LoginComponent/Login.css";
+
+function LoginMe() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>LoginComponent</div>
-  )
+
+ 
+<div style={{ background: '#00000', }}>
+
+
+  <div className="login_box">
+      <h2>CodeReserve</h2>
+      <div className="user_box">
+        <label>Username</label>
+        <input type="text" required=""></input>
+      </div>
+      <div className="user_box">
+        <label>Password</label>
+        <input type="password" required=""></input>
+      </div>
+      <button type="submit">submit</button>
+    </div>
+
+</div>
+   
+    
+          
+);
 }
+
+export default LoginMe;
