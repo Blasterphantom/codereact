46adcc769dc12ad3e02d7b8f1071092f405ffeaa
Blasterphantom <blasterneko@gmail.com>
2023-05-05 10:58:12 -0700

    update

--- src/Components/Services/DataService.js
+++ src/Components/Services/DataService.js
@@ -1,8 +1,9 @@
+
 let userData = {}; 
 
 
 
-async function login(loginUser){
+export async function login(loginUser){
 
     const res = await fetch('https://codewarsreservation.azurewebsites.net/User/AddUser', {
         method: "no-cors",
@@ -24,11 +25,20 @@ console.log(data);
 
 }
 
-async function GetLoggedInUserData(userName){
+export async function GetLoggedInUserData(userName){
     let res = await fetch (`https://codewarsreservation.azurewebsites.net/User/userbyusername/${userName}`)
     let data = await res.json();
     userData = data;
     console.log(userData);
 
     return userData;
+}
+
+export async function GetCodewarUserInfo(){
+    let res = await fetch (`https://www.codewars.com/api/v1/users/Blasterphantom`)
+    let data = await res.json();
+    userData = data;
+    console.log(userData);
+
+    return userData;    
 }
--- src/Components/UserKata/UserCompleted.js
+++ src/Components/UserKata/UserCompleted.js
@@ -9,9 +9,13 @@ import Filter from '../FilterComponent/Filter';
 import NavBarTop from '../NavBarTop/NavBarTop';
 import FooterComponent from '../FooterComponent/FooterComponent';
 import KataCard from '../KataCard/KataCard';
+import { GetCodewarUserInfo } from '../Services/DataService';
 
 export default function UserCompleted() {
+
   const [userInfo, setUserInfo] = useState({});
+
+  let data;
 
   let navigate = useNavigate();
 
@@ -23,6 +27,10 @@ export default function UserCompleted() {
         setUserInfo(JSON.parse(localStorage.getItem('userInfo')));
     }
     
+  }, []);
+
+  useEffect(() => {
+    data = GetCodewarUserInfo();
   }, []);
 
   return (
@@ -42,6 +50,8 @@ export default function UserCompleted() {
 
 
         </Row>
+
+        {/* Marcel was here and crying */}
 
         <FooterComponent />
 
